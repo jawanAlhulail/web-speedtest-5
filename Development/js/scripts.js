936755f2f36bc8ca7e89bea545ce9ecbe29e4461
jawanAlhulail <Jawanalhulail@gmail.com>
2020-07-01 17:25:17 +0300

    done

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -5,7 +5,12 @@ let instructor = {
     username: "careless10",
     numberOfRepos: 200,
   },
-  sayHello: function () {
-    console.log("Hi, I'm Menawer");
+  sayHello: function  () {
+    console.log("menawer");
   },
+
 };
+console.log(instructor.name)
+console.log(instructor["courses"][0]);
+console.log(instructor.githubAccount.username)
+instructor["sayhello"]();
